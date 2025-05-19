127e9df2ba9059e806e46a8439bc1f7c7ee5e6ca
WILLQUERTY <will.developmentdev@gmail.com>
2025-05-19 11:58:30 -0500

    Update pdf

--- src/routes/resume.tsx
+++ src/routes/resume.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { Particle } from "@/components/Particle";
-import pdf from "@/assets/pdfs/JonathanHaywoodResume.pdf?url";
+import pdf from "@/assets/pdfs/WilliamPeñarandaResume.pdf?url";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
