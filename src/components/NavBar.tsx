443e79c5c786252a8a8ad3b8eeaede1065752383
WILLQUERTY <will.developmentdev@gmail.com>
2025-05-19 11:42:27 -0500

    Cambio de nombre

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -97,7 +97,7 @@ export function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/JonHaywood/portfolio"
+                href="https://github.com/WillQUERTY/portfolio"
                 target="_blank"
                 className="fork-btn-inner"
                 title="Portfolio GitHub Repository"
